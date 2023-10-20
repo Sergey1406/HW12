cda623862f447104a1d6403472026c2116f62b48
Sergey1406 <144523192+Sergey1406@users.noreply.github.com>
2023-10-20 20:50:13 +0300

    Add files via upload

---
+++ HW12/src/SingletonLazy.java
@@ -0,0 +1,14 @@
+public class SingletonLazy {
+    private static SingletonLazy INSTANCE;
+
+    private SingletonLazy() {
+    }
+
+    public static SingletonLazy getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new SingletonLazy();
+        }
+        return INSTANCE;
+    }
+}
+
---
+++ HW12/src/SingletonSynchronized.java
@@ -0,0 +1,13 @@
+public class SingletonSynchronized {
+    private static SingletonSynchronized INSTANCE;
+
+    private SingletonSynchronized() {
+    }
+
+    public static synchronized SingletonSynchronized getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new SingletonSynchronized();
+        }
+        return INSTANCE;
+    }
+}
